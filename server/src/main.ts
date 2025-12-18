894dc2e6089dd0fea19a8856a4ab66d3fbb2dffe
Sravan Kumar <sravan.k@astuteverse.com>
2025-12-18 14:00:53 +0530

    Fix port binding for Cloud Run

--- server/src/main.ts
+++ server/src/main.ts
@@ -15,7 +15,8 @@ async function bootstrap() {
   });
 
   // Force Listen on IPv4 (Fixes Windows localhost issues)
-  await app.listen(3000, '0.0.0.0');
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
   console.log(`🚀 Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
