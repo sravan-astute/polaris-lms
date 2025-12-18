9fa73cb8dbca6ecb4fa4c7f5da2dc11f10c386bf
Sravan Kumar <sravan.k@astuteverse.com>
2025-12-18 16:02:03 +0530

    Fix QuestionBuilder props type

--- client/app/components/QuestionBuilder.tsx
+++ client/app/components/QuestionBuilder.tsx
@@ -26,7 +26,7 @@ export interface QuestionData {
 }
 
 interface QuestionBuilderProps {
-  quizId: string;  
+  quizId?: string;  
   onSave: (question: QuestionData) => void;
   onCancel: () => void;
   initialData?: QuestionData;
